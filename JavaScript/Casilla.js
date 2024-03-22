6c082ebf01a80ec55f89e52fe69b445533f003e7
MartiVilas <marti.vilas14@gmail.com>
2024-03-22 16:55:59 +0100

    crear buscaminas

---
+++ JavaScript/Casilla.js
@@ -0,0 +1,29 @@
+
+
+class Casilla {
+    // Propiedades asignadas a la clase Casillas
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.bombasAdyacentes = 0;
+        this.bomba = false;
+        this.descubierta = false;
+        this.bandera = false;
+        this.descubrirCasilla();
+        this.colocarBandera();
+    }
+    
+    // Métodos
+    descubrirCasilla(){
+        if (this.descubierta) {
+            this.descubierta = true;
+        }
+    }
+
+    colocarBandera(){
+        if (this.bandera) {
+            this.bandera = true;
+        }
+    }
+
+}
---
+++ JavaScript/Tablero.js
@@ -0,0 +1,77 @@
+
+
+class Tablero {
+    constructor(filas, columnas, numBombas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.numBombas = numBombas;
+        this.tablero = this.crearTabler();
+        this.ponerBombas();
+        this.calcularAdyacentes();
+    }
+
+    crearTabler() {
+        let array = [];
+        for (let i = 0; i < this.filas; i++) {
+            let fila = [];
+            for (let k = 0; k < this.columnas; k++) {
+                fila.push(new Casilla());
+            }
+            array.push(fila);
+        }
+        return array;
+    }
+
+    ponerBombas() {
+        let contador = 0;
+
+        while (contador < this.numBombas) {
+            let numX = Math.floor(Math.random() * this.filas);
+            let numY = Math.floor(Math.random() * this.columnas);
+
+            if (!this.tablero[numX][numY].bomba) {
+                this.tablero[numX][numY].bomba = true;
+                contador++;
+            }
+        }
+    }
+
+    calcularAdyacentes() {
+        for (let i = 0; i < this.filas; i++) {
+            for (let j = 0; j < this.columnas; j++) {
+                const bombasAdyacentes = this.contarBombas(i, j);
+                this.tablero[i][j].adyacentes = bombasAdyacentes;
+            }
+        }
+    }
+
+    obtenCasillasAdyacentes(x, y) {
+        const adyacentes = [];
+        //Recorrer del -1 al 1 pasando por 0 para sacar las coordenadas
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                const newX = x + i;
+                const newY = y + j;
+
+                if (newX >= 0 && newX < this.filas && newY >= 0 && newY < this.columnas) {
+                    adyacentes.push({ x: newX, y: newY });
+                }
+            }
+        }
+
+        return adyacentes;
+    }
+
+    contarBombas(x, y) {
+        let bombasAdyacentes = 0;
+        const casillasAdyacentes = this.obtenCasillasAdyacentes(x, y);
+
+        for (const casilla of casillasAdyacentes) {
+            if (this.tablero[casilla.x][casilla.y].bomba) {
+                bombasAdyacentes++;
+            }
+        }
+
+        return bombasAdyacentes;
+    }  
+}
